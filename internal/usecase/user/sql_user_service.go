5b90fd0fb7ad695778a4ef1c1b722dff223da260
vanhop993 <71499163+vanhop993@users.noreply.github.com>
2021-10-27 09:48:20 +0700

    push new code

--- internal/usecase/user/sql_user_service.go
+++ internal/usecase/user/sql_user_service.go
@@ -9,6 +9,14 @@ import (
 	"strings"
 )
 
+type UserService interface {
+	GetAll(ctx context.Context) (*[]User, error)
+	Load(ctx context.Context, id string) (*User, error)
+	Insert(ctx context.Context, user *User) (int64, error)
+	Update(ctx context.Context, user *User) (int64, error)
+	Delete(ctx context.Context, id string) (int64, error)
+}
+
 type SqlUserService struct {
 	DB *sql.DB
 }
