e72d3542e35d1cd59e44a8c2db59f21ed1a3ecab
Duc Nguyen <minhduc140583@gmail.com>
2022-05-29 23:45:35 +0700

    Apply latest version

--- go.mod
+++ go.mod
@@ -7,9 +7,9 @@ require (
 	github.com/core-go/health v0.4.7
 	github.com/core-go/log v0.0.8
 	github.com/core-go/search v0.2.5
-	github.com/core-go/service v0.1.9
-	github.com/core-go/sql v0.2.35
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/core-go/service v0.2.1
+	github.com/core-go/sql v0.3.1
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.4
 	github.com/sirupsen/logrus v1.8.1 // indirect
--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -10,7 +10,7 @@ import (
 type User struct {
 	Id           string        `json:"id,omitempty" gorm:"column:id;primary_key" bson:"_id,omitempty" dynamodbav:"id,omitempty" firestore:"id,omitempty" validate:"required,max=40"`
 	Username     string        `json:"username,omitempty" gorm:"column:username" bson:"username,omitempty" dynamodbav:"username,omitempty" firestore:"username,omitempty" validate:"required,username,max=100"`
-	Email        string        `json:"email,omitempty" gorm:"column:email" bson:"emai,omitemptyl" dynamodbav:"email,omitempty" firestore:"email,omitempty" validate:"email,max=100"`
+	Email        string        `json:"email,omitempty" gorm:"column:email" bson:"email,omitempty" dynamodbav:"email,omitempty" firestore:"email,omitempty" validate:"email,max=100"`
 	Phone        string        `json:"phone,omitempty" gorm:"column:phone" bson:"phone,omitempty" dynamodbav:"phone,omitempty" firestore:"phone,omitempty" validate:"required,phone,max=18"`
 	DateOfBirth  *time.Time    `json:"dateOfBirth,omitempty" gorm:"column:date_of_birth" bson:"dateOfBirth,omitempty" dynamodbav:"dateOfBirth,omitempty" firestore:"dateOfBirth,omitempty"`
 	Interests    []string      `json:"interests,omitempty" gorm:"column:interests" bson:"interests,omitempty" dynamodbav:"interests,omitempty" firestore:"interests,omitempty"`
